43a2e83062a44e2f0edfa9879e24b7ffa01fab0c
juangpu <juangabriel.urrutia@outlook.com>
2020-06-29 22:54:58 -0300

    Adicionando nova rota ao arquivo produtos.js

--- routersExpress/controllers/produtosController.js
+++ routersExpress/controllers/produtosController.js
@@ -1,7 +1,11 @@
 const controller = {
     index: (req, res)=>{
         res.send(["Z Nation", "The Society", "Dark"]);
+    },
+    
+    show: (req, res)=>{
+        res.send('Todas as series estão disponiveis no site oficial da Netflix')
     }
-}
+};
 
 module.exports = controller
--- routersExpress/routers/produtos.js
+++ routersExpress/routers/produtos.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/produtosController')
 const router = express.Router()
 
 router.get('/novasSeries', controller.index);
+router.get('/detalhesSeries', controller.show);
 
 module.exports = router
